55ce11aa49e4a89e9ffe79f60681267d75f9e308
jenniferlilli <jenniferlillianlee@gmail.com>
2025-05-20 12:43:45 -0400

    added run instructions

--- startPage.py
+++ startPage.py
@@ -1,4 +1,16 @@
-#http://localhost:5000/login sigh
+'''
+running it on ur own computer
+download files as a zip
+create folder inside called 'templates' and put all the .html files in it
+go to terminal
+cd TechBloom folder path
+if not installed already:
+    - pip install python (??)
+    - pip install flask
+    - pip install sqlalchemy
+run it with python startPage.py
+don't follow terminal link...go to: http://localhost:5000/login
+'''
 import os, uuid, zipfile
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.utils import secure_filename
