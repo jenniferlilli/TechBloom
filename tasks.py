c8e94b8264e9b2dd361e4109b4d7299fc717126d
jenniferlilli <jenniferlillianlee@gmail.com>
2025-07-08 12:26:31 -0400

    deploy

--- tasks.py
+++ tasks.py
@@ -10,7 +10,21 @@ import json
 import uuid
 from celery_app import make_celery
 
-celery = make_celery()
+
+def make_celery():
+    celery = Celery(
+        'tasks',
+        broker=os.environ.get('CELERY_BROKER_URL'),
+        backend=os.environ.get('CELERY_RESULT_BACKEND')
+    )
+    celery.conf.update(
+        task_soft_time_limit=60,
+        task_time_limit=90,
+        worker_concurrency=4,
+        worker_prefetch_multiplier=1,
+        task_acks_late=True,
+    )
+    return celery
 
 bucket_name = 'techbloom-ballots'
 s3_client = boto3.client('s3')
