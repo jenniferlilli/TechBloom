1bcb1a8508f8dec25d2dea0e3d6ded3e4df1d542
Jessika Jetta <127171435+xerixa@users.noreply.github.com>
2025-07-08 14:17:19 -0400

    Update celery_app.py
    i forgot os

--- celery_app.py
+++ celery_app.py
@@ -1,13 +1,23 @@
+import os
 from celery import Celery
 from db_model import DATABASE_URL
+
 print(f"[Celery Worker] Using DATABASE_URL: {DATABASE_URL}")
 
 def make_celery():
+    broker_url = os.environ.get('CELERY_BROKER_URL')
+    backend_url = os.environ.get('CELERY_RESULT_BACKEND')
+
+    if not broker_url:
+        raise ValueError("CELERY_BROKER_URL environment variable not set")
+    # backend can be optional if you don't use results
+
     celery = Celery(
         'tasks',
-        broker=os.environ.get('CELERY_BROKER_URL'),
-        backend=os.environ.get('CELERY_RESULT_BACKEND')
+        broker=broker_url,
+        backend=backend_url
     )
+
     celery.conf.update(
         task_soft_time_limit=60,
         task_time_limit=90,
