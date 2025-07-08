1a85a0ac73296580dffc54ca320c50684605d8fc
Jessika Jetta <127171435+xerixa@users.noreply.github.com>
2025-07-08 14:02:56 -0400

    Update celery_app.py
    udate make clery fucntionn

--- celery_app.py
+++ celery_app.py
@@ -2,17 +2,17 @@ from celery import Celery
 from db_model import DATABASE_URL
 print(f"[Celery Worker] Using DATABASE_URL: {DATABASE_URL}")
 
-def make_celery(app_name=__name__):
+def make_celery():
     celery = Celery(
-        app_name,
-        broker='redis://localhost:6379/0',
-        backend=None 
+        'tasks',
+        broker=os.environ.get('CELERY_BROKER_URL'),
+        backend=os.environ.get('CELERY_RESULT_BACKEND')
     )
     celery.conf.update(
-        task_serializer='json',
-        accept_content=['json'],
-        result_serializer='json',
-        timezone='UTC',
-        enable_utc=True,
+        task_soft_time_limit=60,
+        task_time_limit=90,
+        worker_concurrency=4,
+        worker_prefetch_multiplier=1,
+        task_acks_late=True,
     )
     return celery
